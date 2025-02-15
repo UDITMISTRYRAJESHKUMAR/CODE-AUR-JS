f3629ee3345bec28fa6ab7741fccbce264630489
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-15 19:31:11 +0000

    ok 11

---
+++ video-11/strings.js
@@ -0,0 +1,48 @@
+// console.log("udit");
+const Name = "udit"
+const repoCount = 50
+
+// console.log(Name + repoCount + "value");
+
+
+console.log (`hello this is ${Name} and my repo point is ${repoCount} `)
+
+
+const  gameNew = new String("hitesh-sc")
+
+console.log(gameNew[0]);
+console.log(gameNew.__proto__);
+
+
+
+console.log(gameNew.length);
+console.log(gameNew.toUpperCase());
+console.log(gameNew.charAt(4));
+console.log(gameNew.indexOf("h"));
+
+
+const newString = gameNew.substring(0,4)
+console.log(newString );
+
+
+const anotherString =  gameNew.slice(-8,4)
+console.log(anotherString );
+
+
+const newStringone = "  udit  "
+console.log(newStringone)
+console.log(newStringone.trim())
+
+const url = "https://udi%20t.com/udit"
+
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('%'));
+
+
+console.log(gameNew.split('-'));
+
+
+
+
