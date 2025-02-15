ab69a28dd4d276fcad124d9b757c3cfd4ab32cad
UDITMISTRYRAJESHKUMAR <uditmistryrajeshkumar@gmail.com>
2025-02-15 18:56:09 +0000

    ok 10

---
+++ video-10/stack and heap.js
@@ -0,0 +1,27 @@
+// stack (primitive)  //heap (non premiitivv)
+
+
+// let Name = "udit"
+
+// let nettname = Name
+
+// anothername = "mistry"
+
+// console.log(anothername);
+// console.log(Name);
+
+
+let userOne = {
+    email: "udit@gmail.com",
+    upi: "userubl"
+}
+let userTwo = userOne
+
+
+userTwo.email = "uditmistr@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
+
+
+
+
